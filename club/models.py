0879476ce4bc490888db3c8602f219ecc76b2d36
brian2001dineen-afk <brian2001dineen@gmail.com>
2025-12-02 16:58:35 +0000

    Add club model

--- club/models.py
+++ club/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Private"), (1, "Public"))
+
+class Club(models.Model):
+    """
+    Stores a club created by a user :model:`auth.User`.
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=8, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='clubs')
+    excerpt = models.TextField(blank=False)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
