c8bfa6869280ebab1cb9a2fd569df24f68db57cc
brian2001dineen-afk <brian2001dineen@gmail.com>
2025-12-03 15:57:11 +0000

    add views for paginated club list

--- club/models.py
+++ club/models.py
@@ -1,14 +1,22 @@
-from django.db import models
+import random
+import string
+
 from django.contrib.auth.models import User
+from django.db import models
 
 STATUS = ((0, "Private"), (1, "Public"))
+
+def random_slug(length=8):
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+
 
 class Club(models.Model):
     """
     Stores a club created by a user :model:`auth.User`.
     """
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=8, unique=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    # tags = models.
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='clubs')
     excerpt = models.TextField(blank=False)
@@ -16,3 +24,18 @@ class Club(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
+    unapproved_members = models.ManyToManyField(
+        User, related_name='waitlist', blank=True)
+    approved_members = models.ManyToManyField(
+        User, related_name='club_members', blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = random_slug()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | created by {self.author}"
--- club/views.py
+++ club/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Club
+
 
 # Create your views here.
+
+class ClubList(generic.ListView):
+    queryset = Club.objects.filter(status=1)
+    template_name = "club/clubs.html"
+    paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        clubs = context['object_list']
+        # Add approved member counts for each club
+        context['approved_counts'] = {club.id: club.approved_members.count() for club in clubs}
+        # Or, to access approved members themselves:
+        # context['approved_members'] = {club.id: club.approved_members.all() for club in clubs}
+        return context
+
+def club_detail(request, slug):
+    """
+    Display an individual :model:`club.Club`.
+    """
+
+    queryset = Club.objects.filter(status=1)
+    club = get_object_or_404(queryset, slug=slug)
+    approved_counts = club.approved_members.count()
+    return render(request, 'club_detail.html', {
+        'club': club,
+        'approved_counts': approved_counts,
+    })
